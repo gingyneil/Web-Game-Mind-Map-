fa465e4272d32d2c19c8157e1560dfe3bb4d376f
gingyneil <neil.reading.nr@gmail.com>
2016-06-13 18:10:26 -0400

    Web Game Assignment 2

---
+++ Impress/scripts/game.js
@@ -0,0 +1,6 @@
+/// <reference path="typings/impress/impress.d.ts" />
+function init() {
+    console.log("ok");
+    impress().init();
+}
+//# sourceMappingURL=game.js.map
---
+++ Impress/scripts/game.ts
@@ -0,0 +1,5 @@
+﻿/// <reference path="typings/impress/impress.d.ts" />
+function init() {
+    console.log("ok");
+    impress().init();
+}
